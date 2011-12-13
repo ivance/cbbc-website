038a7c44dbd27fab85b78bf7f9ac877e5506a8ef
Ivance <ivanceyong@gmail.com>
2011-12-13 17:54:40 +0800

    init adding files

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+  <meta charset="utf-8">
+  <title>CBBC Website Control Panel</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <!-- Le styles -->
+  <link href="bootstrap/bootstrap.css" rel="stylesheet">
+  <style type="text/css">
+  body {
+    padding-top: 60px;
+  }
+  .topbar-text{
+    color: #ffffff; 
+  }
+  </style>
+
+</head>
+
+<body>
+
+  <!-- topbar-bof -->
+  <div class="topbar">
+
+    <div class="topbar-inner">
+      <div class="container-fluid">
+        <a class="brand" href="#">CBBC Website Control Panel</a>
+        <p class="pull-right topbar-text">Logged in as username&nbsp;(&nbsp;<a href="#">logout</a>&nbsp;)&nbsp;</p>
+      </div>
+    </div>
+
+  </div>
+  <!-- topbar-eof -->
+
+  <div class="container-fluid">
+    
+    <!-- sidebar-bof -->
+    <div class="sidebar">
+
+      <div class="well">
+        <h5>Pages</h5>
+        <ul>
+          <li><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+        </ul>
+      </div>
+
+    </div>
+    <!-- sidebar-eof -->
+
+    <!-- content-bof -->
+    <div class="content">
+
+      <div class="hero-unit">
+        <h1>CBBC Website Control Panel</h1>
+        <p>Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+        <p><a class="btn primary large">Learn more &raquo;</a></p>
+      </div>
+
+      <div class="row"></div>
+
+      <footer><p>&copy; Ivance & Shawn 2011</p></footer>
+
+    </div>
+    <!-- content-eof -->
+
+  </div>
+
+</body>
+</html>
