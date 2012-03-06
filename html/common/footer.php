1ea15dc79e73c2fecc8e2f1a2d77b4ee91dd2e50
Ivance <ivanceyong@gmail.com>
2012-03-06 18:00:06 +0800

    coding

--- html/common/footer.php
+++ html/common/footer.php
@@ -1,6 +1,6 @@
 	<div id="footer" >
 		<div id="footer-inner" class="container-fluid">
-		<div class="content" style="padding-bottom:40px;">
+		<div class="content" style="padding-bottom:2px;">
 			
 				<p class="tips footerTip"><?php echo $config->common_tip?></p>
 				<p class="footerTip"><?php echo $config->common_copyright?></p>
